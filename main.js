4d0df9d2d725544c0dd73423bc44c4bc36821052
Mohamad Yassine <3058929+moyass@users.noreply.github.com>
2020-12-04 04:39:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+const { app, Menu, Tray, BrowserWindow, screen, globalShortcut} = require('electron');
+const aboutWindow = require('about-window');
+const path = require('path');
+
+function createWindow () {
+  // Check if there are any windows already open.
+  // If so close them
+  let openWindows = BrowserWindow.getAllWindows()
+  if (openWindows.length > 1){
+    openWindows.forEach(function(window){
+      if(window.title == "blackout") window.close();
+    })
+    return
+  }
+
+  // Cover everything that isn't the primary display
+  let mainDisplay = screen.getPrimaryDisplay();
+  let allDisplays = screen.getAllDisplays();
+  
+  let wins = []
+
+  allDisplays.forEach(function(display) {
+    if (mainDisplay.id != display.id){
+      wins += new BrowserWindow ({
+        title: "blackout",
+        width: 10,
+        height:  10,
+        x: display.bounds.x,
+        y: display.bounds.y,
+        alwaysOnTop: true,
+        backgroundColor: "#000",
+        autoHideMenuBar: true,
+        contextIsolation: true,
+        kiosk: true
+      });
+    }
+  })
+}
+
+function showAboutWindow(){
+  aboutWindow({
+    icon_path: path.join(__dirname, 'assets', 'icon.png')
+  });
+}
+let appIcon = null
+app.whenReady().then(()  =>  {
+  const ret = globalShortcut.register('CommandOrControl+4', () => {
+    createWindow();
+  })
+
+  if (!ret) {
+    console.log('registration failed')
+  }
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('CommandOrControl+4'))
+
+
+  
+})
+
+app.on('ready', () => {
+  // Main Window
+  mainWindow = new BrowserWindow ({
+    width: 10,
+    height:  10,
+    show: false
+    
+  })
+
+  appIcon = new Tray(path.join(__dirname, 'assets', 'icon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'About', click() {showAboutWindow()} },
+    { type: 'separator' },
+    { label: 'Toggle Blackout', click() {createWindow()}},
+    { type: 'separator' },
+    { role: 'quit', click() {app.quit()} }
+  ])
+
+  // Call this again for Linux because we modified the context menu
+  appIcon.setContextMenu(contextMenu)
+})
+
+app.on('will-quit', () => {
+  // Unregister a shortcut.
+  globalShortcut.unregister('CommandOrControl+4')
+
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll()
+})
+
